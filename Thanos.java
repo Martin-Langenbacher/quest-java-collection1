867690364bc1a42b18be1f3f4e5bcd4607cdfb52
Martin Langenbacher <martin.langenbacher@googlemail.com>
2019-11-26 21:54:54 +0100

    ML ist fertig

--- Thanos.java
+++ Thanos.java
@@ -5,24 +5,51 @@ public class Thanos {
     public static void main(String[] args) {
 
         // TODO 1 : Create an empty heroes list
-
+    	ArrayList<Hero> heroes = new ArrayList<>();
+   	
+    	
         // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Spider-Man, age: 18
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
-
+    	Hero blackWidow = new Hero ("Black Widow", 34);
+    	Hero captainAmerica = new Hero ("Captain America", 100);
+    	Hero vision = new Hero ("Vision", 3);
+    	Hero ironMan = new Hero ("Iron Man", 48);
+    	Hero scarletWitch = new Hero ("Scarlet Witch", 29);
+    	Hero thor = new Hero ("Thor", 1500);
+    	Hero spiderMan = new Hero ("Spider-Man", 18);
+    	Hero hulk = new Hero ("Hulk", 49);
+    	Hero doctorStrange = new Hero ("Doctor Strange", 42);
+    	
+    	heroes.add(blackWidow);
+    	heroes.add(captainAmerica);
+    	heroes.add(vision);
+    	heroes.add(ironMan);
+    	heroes.add(scarletWitch);
+    	heroes.add(thor);
+    	heroes.add(spiderMan);
+    	heroes.add(hulk);
+    	heroes.add(doctorStrange);
+    	    	
+    	
         // TODO 3 : It's Thor birthday, now he's 1501
+    	thor.setAge(1501);
+    	   	
 
         // TODO 4 : Shuffle the heroes list
+    	 // shuffle or randomize
+        Collections.shuffle(heroes);
+    	
 
-        // TODO 5 : Keep only the half of the list
+        // TODO 5 : Keep only the half (top half) of the list
+        for (int i = 8; i >= 4; i--) {
+        	heroes.remove(i);
+        	}
+        
 
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        for (int i = 0; i < 4; i++) {
+        	Hero standingHero = heroes.get(i); // Get the item i from the list
+        	System.out.println(standingHero.getName()); // This will display the remaining list.
+        	}
+        
     }
 }
